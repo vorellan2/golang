50d4fa819652a38456b677a16b70edfaa8c45208
vorellan2 <mastermindg02@gmail.com>
2020-02-04 19:53:08 -0300

    maps

--- main.go
+++ main.go
@@ -2,30 +2,15 @@ package main
 
 import (
 	"fmt"
-	//"github.com/vorellan/gocurso/maps"
+	"github.com/vorellan/gocurso/maps"
 	//"github.com/vorellan/gocurso/structs"
 
 )
 
 
 func main(){
-	getArray()
-	getSlice()
+	fmt.Println(maps.GetMap("Yohan"))
 
 
 }
 
-func getArray(){
-	var arr1 [2]string
-	arr2 := [3]int{1,2,3}
-	arr1[0] = "array"
-	arr1[1] = "array2"
-	fmt.Println(arr1)
-	fmt.Println(arr2)
-}
-
-func getSlice(){
-	var slice1 []string 
-	slice1 = append ( slice1, "mi", "slice")
-	fmt.Println(slice1)
-}
