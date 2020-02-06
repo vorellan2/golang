7901606d0cee2d6d893c62b34ea6d1df21e61bee
vorellan2 <mastermindg02@gmail.com>
2020-02-06 17:46:41 -0300

    methods

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ type PlatziCareer struct{
 	PlatziCourse
 }
 
+func (p PlatziCourse) Subscribe(name string){
+	fmt.Printf("La persona %s se ha registrado al curso %s\n ", name, p.Name )
+}
+
 func main(){
 	
 	//platziCourse := PlatziCourse{Name: "Go", Slug:"go", Skills: []string{"1","2"}}
@@ -25,6 +29,7 @@ func main(){
 	platziCourse1.Name = "Go1"
 	platziCourse1.Slug = "go1"
 	platziCourse1.Skills = []string{"backend1"}
+	platziCourse1.Subscribe("Yohan")
 	fmt.Println(platziCourse1)
 
 
