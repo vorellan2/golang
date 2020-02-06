89a18c755bf2b1e67a9e8484915959e63cb11849
vorellan2 <mastermindg02@gmail.com>
2020-02-05 21:40:35 -0300

    structs

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
-	"github.com/vorellan/gocurso/maps"
+	//"github.com/vorellan/gocurso/maps"
 	//"github.com/vorellan/gocurso/structs"
 
 )
 
+type PlatziCourse struct{
+	Name string 
+	Slug string
+	Skills []string
+
+}
+
+type PlatziCareer struct{
+	PlatziCourse
+}
 
 func main(){
-	fmt.Println(maps.GetMap("Yohan"))
+	
+	//platziCourse := PlatziCourse{Name: "Go", Slug:"go", Skills: []string{"1","2"}}
+	platziCourse1 := new(PlatziCourse)
+	platziCourse1.Name = "Go1"
+	platziCourse1.Slug = "go1"
+	platziCourse1.Skills = []string{"backend1"}
+	fmt.Println(platziCourse1)
 
 
 }
