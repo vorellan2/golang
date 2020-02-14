01be013d93d8f8ca0628b13f254b8504c71bea27
vorellan2 <mastermindg02@gmail.com>
2020-02-14 16:42:38 -0300

    interfaces

--- main.go
+++ main.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	//"github.com/vorellan/gocurso/maps"
-	//"github.com/vorellan/gocurso/structs"
+	"github.com/vorellan/gocurso/structs"
 
 )
 
-type PlatziCourse struct{
-	Name string 
-	Slug string
-	Skills []string
 
-}
-
-type PlatziCareer struct{
-	PlatziCourse
-}
-
-func (p PlatziCourse) Subscribe(name string){
-	fmt.Printf("La persona %s se ha registrado al curso %s\n ", name, p.Name )
-}
 
 func main(){
 	
-	//platziCourse := PlatziCourse{Name: "Go", Slug:"go", Skills: []string{"1","2"}}
-	platziCourse1 := new(PlatziCourse)
-	platziCourse1.Name = "Go1"
-	platziCourse1.Slug = "go1"
-	platziCourse1.Skills = []string{"backend1"}
-	platziCourse1.Subscribe("Yohan")
-	fmt.Println(platziCourse1)
+	structs.InterfaceTest()
 
 
 }
